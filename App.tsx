2bef7070ced79f2a1e8dce9a3c25c22b1402880f
Evandro Silva <evandroagostinho.silva@equilibrium.co.ao>
2023-04-25 21:24:55 +0100

    set App() backgroundColor

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <>
+    <View style={{ flex: 1, backgroundColor: '#1A1A1A' }}>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
       <Home />
-    </>
+    </View>
   );
 }
